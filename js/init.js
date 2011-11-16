b3896ff63ab12546ae8442ac1fb0d66a62b5c36f
Smotko <ensmotko@gmail.com>
2011-11-16 21:13:33 +0100

    Added sliders. Sliders!

--- js/init.js
+++ js/init.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
+
+	$( ".slider" ).slider({
+		value:1,
+		min: 1,
+		max: 10,
+		step: 1,
+		slide: function( event, ui ) {
+			console.log(ui.value + " " + ui.id);
+		}
+	});
 	
-	$( "#slider" ).slider();
 	$("select").chosen();
 	$( "#tabs" ).tabs();
 });
