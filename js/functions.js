01a782adac503df6e5b8d78844baecf6f7522266
Smotko <ensmotko@gmail.com>
2011-11-17 00:54:45 +0100

    Interface for multiple tracks/tapes. Not working yet.

--- js/functions.js
+++ js/functions.js
@@ -8,5 +8,36 @@ handle_slider = function(event, ui){
 			}
 			states.trigger("liszt:updated");
 			break;
+			
+		case('num_tapes'):
+			var track = $('#track');
+			track.siblings().remove();
+			new_track = track.clone();
+			new_track.find('#state_in,#state_out').parent().html('');
+			new_track.find('#add_instruction').remove();
+			console.log(new_track);
+			
+			for(var i=1; i < ui.value; i++){
+				track.parent().append(new_track.clone());
+			}
+		
+		break;
+		case('num_trails'):
+			var trail_input = $('#input_trails');
+			trail_input.empty();
+
+			for(var i=0; i < ui.value; i++){					
+				trail_input.append('<select class="input" id="'+trail_input.attr('id')+'_'+i+'"><option>0</option><option>1</option></select>');
+			}
+			var trail_output = $('#output_trails');
+			trail_output.empty();
+
+			for(var i=0; i < ui.value; i++){					
+				trail_output.append('<select class="input" id="'+trail_output.attr('id')+'_'+i+'"><option>0</option><option>1</option></select>');
+			}
+
+			$('.input').chosen();
+			break;
+			
 	};
 };
