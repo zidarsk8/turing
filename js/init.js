1c9cb45d697d357acc63e8926d1efbae8acb1b89
HairyFotr <maticpotocnik@gmail.com>
2011-11-21 16:12:42 +0100

    navigation, some graphics

--- js/init.js
+++ js/init.js
@@ -1,5 +1,16 @@
+var  graph; // probably doing it wrong...
+
 $(document).ready(function() {
 
+    // navigation
+    $('nav > a').click(function(a) {
+        $('.visiblepane').removeClass('visiblepane');
+        $('#'+this.id.substring(3)).addClass('visiblepane');
+        $('.selectednav').removeClass('selectednav');
+        $(this).addClass('selectednav');
+    })
+
+    // settings init
 	$(".slider").slider({
 		value:1,
 		min: 1,
@@ -16,4 +27,13 @@ $(document).ready(function() {
 	
 	turing_states = [];
 	update_states_table();
+    
+    //graphics init
+    graph = new Raphael(document.getElementById('graphcanvas'), "100%", "500px");
+    $('#graphcanvas').click(function(e) {
+        graph.circle(e.pageX - this.offsetLeft,
+                          e.pageY - this.offsetTop,
+                          20).attr({"fill":"#fff"});
+    })
+    
 });
