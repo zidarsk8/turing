6fc0e3ceb91c993b7a93720e22aa584bc5b0d8d3
Miha Zidar <zidarsk8@gmail.com>
2011-12-27 18:26:00 +0100

    automatic graph update on track change and stuff

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -2,6 +2,7 @@ handle_slider = function(event, ui){
 	turing[this.id] = ui.value;
 	$("#"+this.id+"Label").html(ui.value+" "+this.id.substr(3,this.id.length-4+(ui.value>1)))
 	turing[this.id] = ui.value;
+	$("#turingCodeEditor").keyup()
 };
 
 
