cc9b4e10e030428b4bfde6f24dc1b971d3cfb3a0
Miha Zidar <zidarsk8@gmail.com>
2011-12-27 15:50:36 +0100

    fixed graph bugzzz

--- js/graph.js
+++ js/graph.js
@@ -8,27 +8,42 @@ var graph = {
 		
 		console.log("should be drawing graph")
 		
-		for (var i in states){
-			this.graph.circle(states[i].x, states[i].y, 20).attr({"stroke-width":"2px","fill":"#fff"})
-			this.graph.text(states[i].x,states[i].y,i).attr({"font":"16px serif", "text-anchor":"middle","fill":"#001111"});
-		}
 		for (var d in deltas){
 			var sin = deltas[d].fromState
 			var sout = deltas[d].toState
+			var x1 = states[sin].x
+			var y1 = states[sin].y
+			var x2 = states[sout].x
+			var y2 = states[sout].y
+			var xy = this.shorten_line(x1,y1,x2,y2, 22)
+			var x2s=xy[0], y2s=xy[1]
 
-			var xy = this.shorten_line(states[sin].x,states[sin].y,states[sout].x,states[sout].y, 22)
-			var x=xy[0], y=Math.floor(xy[1])
+			//var minDistance = 30;
+			//if (Math.abs(x1-x2) < minDistance  && Math.abs(y1-y2) < minDistance){
+			//	console.log("naredi krogec")
+			//}
 
-			this.graph.arrow(states[sin].x, states[sin].y, x, y, 10).attr({"stroke-width":"2px"})
+			this.graph.arrow(states[sin].x, states[sin].y, x2s, y2s, 10).attr({"stroke-width":"2px"})
 			
-			x = (x+states[sin].x)/2
-			y = (y+states[sin].y)/2
+			
+			var len = this.getLength(x1,y1,x2,y2)
+			
+			var tx = x2 - len/2
+			var ty = y2 
+
+			var r = this.rotatePoints(tx,ty,x2,y2, Raphael.rad(Raphael.angle(x2,y2,x1,y1)+30))
+			tx = r.x
+			ty = r.y
 			
 			var fromS =  _.reduce(_.flatten(deltas[d].fromSymbol),function(a,b){return a+" "+b} )
 			var toS =  _.reduce(_.flatten(deltas[d].toSymbol),function(a,b){return a+" "+b} )
 			var move =  _.reduce(_.flatten(deltas[d].move),function(a,b){return a+" "+b} )
-			this.graph.text(x,y-15,fromS+" -> "+toS+","+move).
+			this.graph.text(tx,ty,fromS+" -> "+toS+","+move).
 					attr({"font":"16px serif", "text-anchor":"middle","fill":"#001111"});
+		}
+		for (var i in states){
+			this.graph.circle(states[i].x, states[i].y, 20).attr({"stroke-width":"2px","fill":"#fff"})
+			this.graph.text(states[i].x,states[i].y,i).attr({"font":"16px serif", "text-anchor":"middle","fill":"#001111"});
 		}
 		
 	},
@@ -38,13 +53,19 @@ var graph = {
 				y: Math.sin(theta) * (px-ox) + Math.cos(theta) * (py-oy) + oy}
 	},
 
+	getLength : function(x1,y1,x2,y2){
+		var dx = x2-x1;
+		var dy = y2-y1;
+		return Math.sqrt(dx*dx + dy*dy);
+	},
+
 	shorten_line : function(x1,y1,x2,y2,d) {
 		var r = this.rotatePoints(x1,y1,x2,y2,Raphael.rad(30))
 		x1 = r.x
 		y1 = r.y
 		var dx = x2-x1;
 		var dy = y2-y1;
-		var length = Math.sqrt(dx*dx + dy*dy);
+		var length = this.getLength(x1,y1,x2,y2)
 		if(length > 0) {
 			dx /= length;
 			dy /= length;
@@ -58,26 +79,16 @@ var graph = {
 
 
 Raphael.fn.arrow = function(x1, y1, x2, y2, size) {
-	var minDistance = 30;
 	var angle = Raphael.angle(x1, y1, x2, y2);
 	var a45   = Raphael.rad(angle-45+30);
 	var a45m  = Raphael.rad(angle+45+30);
-	var a135  = Raphael.rad(angle-135);
-	var a135m = Raphael.rad(angle+135);
-	var x1a = x1 + Math.cos(a135) * size;
-	var y1a = y1 + Math.sin(a135) * size;
-	var x1b = x1 + Math.cos(a135m) * size;
-	var y1b = y1 + Math.sin(a135m) * size;
 	var x2a = x2 + Math.cos(a45) * size;
 	var y2a = y2 + Math.sin(a45) * size;
 	var x2b = x2 + Math.cos(a45m) * size;
 	var y2b = y2 + Math.sin(a45m) * size;
-	var r = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)+(y1-y2));
-	if (Math.abs(x1-x2) < minDistance  && Math.abs(y1-y2) < minDistance){
-		console.log("naredi krogec")
-	}
+	var r = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
 	return this.path(
-			"M"+x1+" "+y1+"A"+" "+r+" "+r+" 100 0 1 " +x2+" "+y2+
+			"M"+x1+" "+y1+"A"+" "+r+" "+r+" -80 0 1 " +x2+" "+y2+
 			"M"+x2+" "+y2+"L"+x2a+" "+y2a+
 			"M"+x2+" "+y2+"L"+x2b+" "+y2b
 			);
